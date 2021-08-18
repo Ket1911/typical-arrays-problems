ddde59dfb7441a995999a19bb3ce34f89d5c0be0
Katerina <kostiukkaterina@gmail.com>
2021-08-18 09:17:19 +0300

    init: add index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (typeof array === 'undefined' || array.length === 0) {
+    return 0;
+  } else {
+  
+    let minValue = array[0];
+  array.forEach( function(item, i){
+    if (item < minValue) {
+      minValue = item;
+    }
+  });
+  return minValue;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (typeof array === 'undefined' || array.length === 0) {
+    return 0;
+  } else {
+    let maxValue = array[0];
+    array.forEach( function(item, i){
+      if (item > maxValue) {
+        maxValue = item;
+    }
+  });
+  return maxValue;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (typeof array === 'undefined' || array.length === 0) {
+    return 0;
+    } else {
+      let avgValue = 0;
+      array.forEach( function(item, i){
+        avgValue = avgValue + item;
+    });
+
+  
+  return avgValue / array.length;
+  }
 }
